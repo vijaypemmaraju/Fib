2eaaaedbf2be5ef2c6b752813e7e808125dc441e
James Yanyuk <jyanyuk@gmail.com>
2015-02-07 13:41:20 -0500

    Fixed setup

--- Fib/AppDelegate.swift
+++ Fib/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  Fib
 //
 //  Created by James Yanyuk on 2/7/15.
-//  Copyright (c) 2015 James Yanyuk. All rights reserved.
+//  Copyright (c) 2015 hackbeanpot. All rights reserved.
 //
 
 import UIKit
